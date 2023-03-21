5cff4859806a6948f7b47a36fabef95c37d0bb8f
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-03-21 13:03:45 +0100

    Por terminar

--- Tema7-Ejercicios2/src/ejercicio3/Fecha.java
+++ Tema7-Ejercicios2/src/ejercicio3/Fecha.java
@@ -4,15 +4,15 @@ public class Fecha {
 	private int dia;
 	private int mes;
 	private int año;
-	
-	public Fecha () {
-		
+
+	public Fecha() {
+
 	}
-	
-	public Fecha (int dia, int mes, int año) {
-		this.dia=dia;
-		this.mes=mes;
-		this.año=año;
+
+	public Fecha(int dia, int mes, int año) {
+		this.dia = dia;
+		this.mes = mes;
+		this.año = año;
 	}
 
 	public int getDia() {
@@ -38,15 +38,100 @@ public class Fecha {
 	public void setAño(int año) {
 		this.año = año;
 	}
-	
-	boolean esBisiesto () {
+
+	boolean esBisiesto() {
 		boolean bisiesto = false;
-		if (año%4==0) {
-			bisiesto=true;
+		if (año % 4 == 0) {
+			bisiesto = true;
 		}
-		
+
 		return bisiesto;
 	}
+
+	boolean fechaCorrecta() {
+		boolean estado = false;
+		switch (mes) {
+		case 1: {
+			if (dia > 0 && dia < 32) {
+				estado = true;
+			}
+		}
+
+		case 2: {
+			if (esBisiesto()) {
+				if (dia > 0 && dia < 30) {
+					estado = true;
+				}
+			} else if (dia > 0 && dia < 29) {
+				estado = true;
+			}
+		}
+
+		case 3: {
+			if (dia > 0 && dia < 32) {
+				estado = true;
+			}
+		}
+		
+		case 4: {
+			if (dia > 0 && dia < 31) {
+				estado = true;
+			}
+		}
+		
+		case 5: {
+			if (dia > 0 && dia < 32) {
+				estado = true;
+			}
+		}
+		
+		case 6: {
+			if (dia > 0 && dia < 31) {
+				estado = true;
+			}
+		}
+		
+		case 7: {
+			if (dia > 0 && dia < 32) {
+				estado = true;
+			}
+		}
+		
+		case 8: {
+			if (dia > 0 && dia < 32) {
+				estado = true;
+			}
+		}
+		
+		case 9: {
+			if (dia > 0 && dia < 31) {
+				estado = true;
+			}
+		}
+		
+		case 10: {
+			if (dia > 0 && dia < 32) {
+				estado = true;
+			}
+		}
+		
+		case 11: {
+			if (dia > 0 && dia < 31) {
+				estado = true;
+			}
+		}
+		
+		case 12: {
+			if (dia > 0 && dia < 32) {
+				estado = true;
+			}
+		}
+		}
+		
+		return estado;
+	}
 	
-	
+	void diaSiguiente() {
+		
+	}
 }
