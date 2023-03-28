665ede893c466bc16db77e281fa68365b8268086
Fernando <Fernando@DESKTOP-3EE8PP0.home>
2023-03-28 22:21:29 +0200

    Terminado y comentado

--- Tema7-Ejercicios2/src/ejercicio3/Fecha.java
+++ Tema7-Ejercicios2/src/ejercicio3/Fecha.java
@@ -1,44 +1,104 @@
 package ejercicio3;
 
+/**
+ * Esta clase guarda una fecha con su día, mes y año. Comprobando si la fecha es
+ * correcta, según los días que tenga cada mes y si el año es bisiesto o no,
+ * para el caso de Febrero. Se incrementará un día a la fecha y se modificará
+ * esta correctamente
+ */
 public class Fecha {
+	/**
+	 * día del mes
+	 */
 	private int dia;
+	/**
+	 * mes del año
+	 */
 	private int mes;
+	/**
+	 * año de la fecha
+	 */
 	private int año;
 
+	/**
+	 * Constructor vacío
+	 */
 	public Fecha() {
 
 	}
 
+	/**
+	 * Constructor al que se le asigna valores a dia, mes y año de la fech
+	 * 
+	 * @param dia del mes
+	 * @param mes del año
+	 * @param año de la fecha
+	 */
 	public Fecha(int dia, int mes, int año) {
 		this.dia = dia;
 		this.mes = mes;
 		this.año = año;
 	}
 
+	/**
+	 * Este método devuelve el valor actual de dia
+	 * 
+	 * @return el dia
+	 */
 	public int getDia() {
 		return dia;
 	}
 
+	/**
+	 * Este método actualiza el dia
+	 * 
+	 * @param dia a actualizar
+	 */
 	public void setDia(int dia) {
 		this.dia = dia;
 	}
 
+	/**
+	 * Este método devuelve el valor actual del mes
+	 * 
+	 * @return el mes
+	 */
 	public int getMes() {
 		return mes;
 	}
 
+	/**
+	 * Este método actualiza el mes
+	 * 
+	 * @param mes a actualizar
+	 */
 	public void setMes(int mes) {
 		this.mes = mes;
 	}
 
+	/**
+	 * Este método devuelve el valor actual del año
+	 * 
+	 * @return el año
+	 */
 	public int getAño() {
 		return año;
 	}
 
+	/**
+	 * Este método actualiza el año
+	 * 
+	 * @param año a actualizar
+	 */
 	public void setAño(int año) {
 		this.año = año;
 	}
 
+	/**
+	 * Este método comprueba si el año de la fecha es bisiesto o no
+	 * 
+	 * @return true en caso de ser bisiesto y false en caso de no serlo
+	 */
 	private boolean esBisiesto() {
 		boolean bisiesto = false;
 		if (año % 4 == 0) {
@@ -48,10 +108,16 @@ public class Fecha {
 		return bisiesto;
 	}
 
+	/**
+	 * Este método comprueba si el dia del mes es correcto o no, dependiendo de cada
+	 * mes
+	 * 
+	 * @return true si es correcto y false si no lo es
+	 */
 	boolean fechaCorrecta() {
 		boolean estado = false;
 		switch (mes) {
-		case 1: {
+		case 1, 3, 5, 7, 8, 10, 12: {
 			if (dia > 0 && dia < 32) {
 				estado = true;
 			}
@@ -65,92 +131,89 @@ public class Fecha {
 			} else if (dia > 0 && dia < 29) {
 				estado = true;
 			}
+
 		}
 
-		case 3: {
-			if (dia > 0 && dia < 32) {
-				estado = true;
-			}
-		}
-
-		case 4: {
+		case 4, 6, 9, 11: {
 			if (dia > 0 && dia < 31) {
 				estado = true;
 			}
+
 		}
 
-		case 5: {
-			if (dia > 0 && dia < 32) {
-				estado = true;
-			}
 		}
-
-		case 6: {
-			if (dia > 0 && dia < 31) {
-				estado = true;
-			}
-		}
-
-		case 7: {
-			if (dia > 0 && dia < 32) {
-				estado = true;
-			}
-		}
-
-		case 8: {
-			if (dia > 0 && dia < 32) {
-				estado = true;
-			}
-		}
-
-		case 9: {
-			if (dia > 0 && dia < 31) {
-				estado = true;
-			}
-		}
-
-		case 10: {
-			if (dia > 0 && dia < 32) {
-				estado = true;
-			}
-		}
-
-		case 11: {
-			if (dia > 0 && dia < 31) {
-				estado = true;
-			}
-		}
-
-		case 12: {
-			if (dia > 0 && dia < 32) {
-				estado = true;
-			}
-		}
-		}
-
 		return estado;
 	}
 
+	/**
+	 * Este método incrementa el día de la fecha en 1 y actualiza la fecha completa
+	 * a la correspondiente después del incremento
+	 */
 	void diaSiguiente() {
 		dia++;
 		fechaCorrecta();
+
+		switch (mes) {
+		case 1, 3, 5, 7, 8, 10, 12: {
+			if (dia == 32) {
+				dia = 1;
+				mes++;
+			}
+
+			if (mes == 13) {
+				mes = 1;
+				año++;
+			}
+
+			break;
+		}
+		case 4, 6, 9, 11: {
+			if (dia == 31) {
+				dia = 1;
+				mes++;
+			}
+
+			break;
+		}
+
+		case 2: {
+			if (esBisiesto()) {
+				if (dia == 30) {
+					dia = 1;
+					mes++;
+				}
+			}
+
+			else if (dia >= 29) {
+				dia = 1;
+				mes++;
+			}
+		}
+
+		}
+
 	}
 
+	/**
+	 * Este método devuelve el resultado de la fecha final, con su día, mes y año.
+	 * Teniendo en cuenta de que si el día o el mes es menor a 10, se deben mostrar
+	 * con un 0 delante
+	 */
 	public String toString() {
 		String result = "";
 		if (dia < 10) {
-			result = "0" + dia + "-"  + mes + "-" + año;
+			result = "0" + dia + "-" + mes + "-" + año;
 		}
-		
-		if (mes<10) {
-			result =  dia + "-" + "0" + mes + "-" + año;
+
+		if (mes < 10) {
+			result = dia + "-" + "0" + mes + "-" + año;
 		}
-		
+
 		if (dia < 10 && mes < 10) {
 			result = "0" + dia + "-" + "0" + mes + "-" + año;
 		}
 
-		if (dia>9 && mes>9) {
+		if (dia > 9 && mes > 9) {
 			result = dia + "-" + mes + "-" + año;
 		}
 
--- Tema7-Ejercicios2/src/ejercicio3/Main.java
+++ Tema7-Ejercicios2/src/ejercicio3/Main.java
@@ -3,11 +3,19 @@ package ejercicio3;
 public class Main {
 
 	public static void main(String[] args) {
-		Fecha objeto = new Fecha(28, 2, 2023);
-		
+
+		// Objeto, al que se le pasa como parámetro al constructor un día, un mes y un
+		// año
+		Fecha objeto = new Fecha(31, 12, 2028);
+
+		// Llamamos al método que comprueba si la fecha introducida es correcta
 		objeto.fechaCorrecta();
+
+		// Llamamos al método que incrementa el día de la fecha en 1 y la actualiza
 		objeto.diaSiguiente();
-		System.out.println(objeto.toString()); 
+
+		// Imprimimos la fecha actual final
+		System.out.println(objeto.toString());
 
 	}
 
